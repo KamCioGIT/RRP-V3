8d86aa086611b1b6cf9a181f45b4ed1e37c49f1b
Skutela32 - Laptop <96396382+Skutela32@users.noreply.github.com>
2024-02-01 20:47:41 +0000

    Update materials.lua

--- [TI]/ti_vehicleTuning/data/materials.lua
+++ [TI]/ti_vehicleTuning/data/materials.lua
@@ -10,42 +10,42 @@ Materials = {
         label = "Aluminum",
         strength = 0.3,
         weight = 0.25,
-        prestige = 0.15,
+        prestige = 0.075,
     },
 
     forgedSteel = {
         label = "Forged Steel",
         strength = 0.7,
         weight = 1.0,
-        prestige = 0.25,
+        prestige = 0.125,
     },
 
     titanium = {
         label = "Titanium",
         strength = 0.6,
         weight = 0.4,
-        prestige = 0.3,
+        prestige = 0.15,
     },
 
     super = {
         label = "Super",
         strength = 1.5,
         weight = 0.2,
-        prestige = 0.6
+        prestige = 0.3
     },
 
     unobtanium = {
         label = "Unobtanium",
         strength = 3.5,
         weight = 0.1,
-        prestige = 1.25
+        prestige = -.75
     },
 
     indestructible = {
         label = "Indestructible",
         strength = 6.0,
         weight = 0.0,
-        prestige = 3.0
+        prestige = 1.5
     }
 }
 
