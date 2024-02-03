5f60ecbafaea4d7b63dbe49ab56d9cdfa577361a
Skutela32 <kwebalkop@gmail.com>
2024-02-03 13:43:03 +0000

    Scripts

--- [core]/[renewed]/qb-phone/server/employment.lua
+++ [core]/[renewed]/qb-phone/server/employment.lua
@@ -20,7 +20,7 @@ local function getJobs(cid)
     return jobs, employees
 end exports('getJobs', getJobs)
 
-local FirstStart = false
+local FirstStart = true
 
 CreateThread(function()
     ---- Convertion Tool I guess LOL ----
