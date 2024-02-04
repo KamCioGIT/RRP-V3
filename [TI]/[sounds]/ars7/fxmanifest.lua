a09e7179cec08326dfb2118be87c49ddf272307a
Skutela32 <kwebalkop@gmail.com>
2024-02-04 01:30:27 +0000

    fix crashes

---
+++ [TI]/[sounds]/ars7/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+files {
+  'audioconfig/ars7_game.dat151.rel',
+  'audioconfig/ars7_sounds.dat54.rel',
+  'sfx/dlc_ars7/ars7.awc',
+  'sfx/dlc_ars7/ars7_npc.awc'
+}
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/ars7_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/ars7_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_ars7'
