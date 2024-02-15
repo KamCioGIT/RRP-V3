fd713b74291e2a218c3fd9f8b462cda2f307635c
Skutela32 <kwebalkop@gmail.com>
2024-02-15 00:29:28 +0000

    Added Dildo + Placeable Items

--- [core]/qb-core/shared/items.lua
+++ [core]/qb-core/shared/items.lua
@@ -610,4 +610,7 @@ sparetire                   = { name = "sparetire", label = "Spare Tire", weight
     --['milk_pail']              	= {['name'] = 'milk_pail',               	['label'] = 'Milk Pail',       			['weight'] = 1000,      ['type'] = 'item',      ['image'] = 'milk_pail.png',        		['unique'] = false,     ['useable'] = true,     ['shouldClose'] = false,    ['combinable'] = nil,   ['description'] = 'Fresh milk, straight from the cow!'},
     ['milk']              	 	= {['name'] = 'milk',               		['label'] = 'Milk',                		['weight'] = 250,       ['type'] = 'item',      ['image'] = 'milk.png',        				['unique'] = false,     ['useable'] = false,    ['shouldClose'] = false,    ['combinable'] = nil,   ['description'] = 'A bottle of fresh milk!'},
     
+    --dildo
+    ['dildo']              	 	= {['name'] = 'dildo',               		['label'] = 'Dildo',                    ['weight'] = 250,       ['type'] = 'item',      ['image'] = 'dildo.png',        				['unique'] = false,     ['useable'] = false,    ['shouldClose'] = false,    ['combinable'] = nil,   ['description'] = 'Eww Its Sticky'},
+    
 }
--- [standalone]/[KQ]/kq_placeable_items/config.lua
+++ [standalone]/[KQ]/kq_placeable_items/config.lua
@@ -94,80 +94,478 @@ Config.allowPlacingOnRoofs = true
 -- All placeable items with the amounts and props defined
 -- https://gta-objects.xyz/objects
 Config.items = {
-
+    --dildo
+    ['dildo'] = {
+        [1] = 'prop_cs_dildo_01',
+    },
     ['phone'] = {
         [1] = 'prop_player_phone_01',
     },
-    ['laptop'] = {
+	['laptop'] = {
         [1] = 'prop_laptop_02_closed',
-    },
-
-    --weapons
-    ['weapon_bat'] = {
+	},
+	--weapons
+	['weapon_bat'] = {
         [1] = 'p_cs_bbbat_01',
-    },
-    ['weapon_crowbar'] = {
+	},
+	['weapon_crowbar'] = {
         [1] = 'prop_ing_crowbar',
-    },
-    ['weapon_pistol'] = {
+	},
+	['weapon_pistol'] = {
         [1] = 'w_pi_pistol',
-    },
-    ['weapon_pistolxm3'] = {
+	},
+	['weapon_pistolxm3'] = {
         [1] = 'w_pi_heavypistol',
-    },
-    ['weapon_petrolcan'] = {
+	},
+	['weapon_petrolcan'] = {
         [1] = 'w_am_jerrycan',
-    },
-    ['clip_attachment'] = {
+	},
+	['clip_attachment'] = {
         [1] = 'w_pi_pistol_mag1',
-    },
-    ['clip_attachment'] = {
+	},
+	['clip_attachment'] = {
         [1] = 'w_pi_pistol_mag1',
-    },
-
-    --ammo
-    ['pistol_ammo'] = {
+	},
+	--ammo
+	['pistol_ammo'] = {
         [1] = 'prop_ld_ammo_pack_01',
-    },
-
-    --drink
-    ['water_bottle'] = {
+	},
+	--drink
+	['water_bottle'] = {
         [1] = 'apa_mp_h_acc_bottle_01',
-    },
-    ['coffee'] = {
+	},
+	['coffee'] = {
         [1] = 'p_ing_coffeecup_01',
-    },
-    ['kurkakola'] = {
+	},
+	['kurkakola'] = {
         [1] = 'prop_ecola_can',
-    },
-    ['beer'] = {
+	},
+	['beer'] = {
         [1] = 'prop_beer_am',
-    },
-    ['whiskey'] = {
+	},
+	['whiskey'] = {
         [1] = 'prop_whiskey_bottle',
-    },
-    ['vodka'] = {
+	},
+	['vodka'] = {
         [1] = 'prop_vodka_bottle',
-    },
-    ['grape'] = {
+	},
+	['grape'] = {
         [1] = 'p_kitch_juicer_s',
-    },
-    ['wine'] = {
+	},
+	['wine'] = {
         [1] = 'prop_wine_rose',
-    },
-    ['rolling_paper'] = {
+	},
+	['grapejuice'] = {
+        [1] = 'ng_proc_brkbottle_02a',
+	},
+	--Drugs
+	['joint'] = {
+        [1] = 'prop_sh_joint_01',
+	},
+	['cokebaggy'] = {
+        [1] = 'prop_coke_block_half_b',
+	},
+	['crack_baggy'] = {
+        [1] = 'prop_coke_block_half_a',
+	},
+	['xtcbaggy'] = {
+        [1] = 'ex_office_swag_pills1',
+	},
+	['coke_brick'] = {
+        [1] = 'bkr_prop_coke_block_01a',
+	},
+	['weed_brick'] = {
+        [1] = 'hei_prop_heist_weed_block_01b',
+	},
+	['coke_small_brick'] = {
+        [1] = 'imp_prop_impexp_boxcoke_01',
+	},
+	['oxy'] = {
+        [1] = 'hei_prop_pill_bag_01',
+	},
+	['meth'] = {
+        [1] = 'bkr_prop_meth_smallbag_01a',
+	},
+	['rolling_paper'] = {
         [1] = 'p_cs_papers_02',
-    },
-    ['tunerlaptop'] = {
+	},
+	--Weed 
+	['weed_purplehaze'] = {
+        [1] = 'bkr_prop_weed_smallbag_01a',
+	},
+	['empty_weed_bag'] = {
+        [1] = 'bkr_prop_weed_bag_01a',
+	},
+	['weed_nutrition'] = {
+        [1] = 'bkr_prop_weed_spray_01a',
+	},
+	--Material
+	['plastic'] = {
+        [1] = 'prop_yell_plastic_target',
+	},
+	['metalscrap'] = {
+        [1] = 'prop_ld_scrap',
+	},
+	['glass'] = {
+        [1] = 'gr_dlc_gr_yacht_props_glass_04',
+	},
+	--Tools
+	['drill'] = {
+        [1] = 'gr_prop_gr_drill_01a',
+	},
+	['screwdriverset'] = {
+        [1] = 'prop_tool_screwdvr01',
+	},
+	-- Vehicle Tools
+	['repairkit'] = {
+        [1] = 'prop_tool_box_04',
+	},
+	['advancedrepairkit'] = {
+        [1] = 'imp_prop_tool_chest_01a',
+	},
+	['cleaningkit'] = {
+        [1] = 'prop_sponge_01',
+	},
+	['tunerlaptop'] = {
         [1] = 'gr_prop_gr_laptop_01c',
+	},
+	['harness'] = {
+        [1] = 'p_mrk_harness_s',
+	},
+	['jerry_can'] = {
+        [1] = 'prop_jerrycan_01a',
+	},
+	['tirerepairkit'] = {
+        [1] = 'imp_prop_wheel_balancer_01a',
+	},
+	-- Mechanic Parts
+	['veh_toolbox'] = {
+        [1] = 'imp_prop_tool_cabinet_01b',
+	},
+	['veh_engine'] = {
+        [1] = 'prop_car_engine_01',
+	},
+	['veh_transmission'] = {
+        [1] = 'imp_prop_impexp_gearbox_01',
+	},
+	['veh_exterior'] = {
+        [1] = 'imp_prop_impexp_parts_rack_02a',
+	},
+	['veh_wheels'] = {
+        [1] = 'imp_prop_impexp_wheel_03a',
+	},
+	['veh_plates'] = {
+        [1] = 'p_num_plate_02',
+	},
+	-- Medication
+	['firstaid'] = {
+        [1] = 'xm_prop_x17_bag_med_01a',
+	},
+	['bandage'] = {
+        [1] = 'prop_med_bag_01',
+	},
+	['painkillers'] = {
+        [1] = 'v_med_bottles3',
+	},
+	['walkstick'] = {
+        [1] = 'prop_cs_walking_stick',
+	},
+	-- Communication
+	['phone'] = {
+        [1] = 'prop_amb_phone',
+	},
+	['radio'] = {
+        [1] = 'prop_cs_hand_radio',
+	},
+	['laptop'] = {
+        [1] = 'p_cs_laptop_02_w',
+	},
+	['tablet'] = {
+        [1] = 'bkr_prop_fakeid_tablet_01a',
+	},
+	-- Theft and Jewelry
+	['rolex'] = {
+        [1] = 'p_watch_06',
+	},
+	['goldbar'] = {
+        [1] = 'hei_prop_heist_gold_bar',
+	},
+    --animals
+    ['milk'] = {
+        [1] = 'v_res_tt_milk',
     },
+    ['egg'] = {
+        [1] = 'v_ret_247_eggs',
+    ],
+--fruits
+
+    ['pineapple'] = {
+        [1] = 'prop_pineapple',
+    ],
+	
+	['lemon'] = {
+        [1] = 'prop_bar_lemons',
+    ],
+	
+	['cherry'] = {
+        [1] = 'apa_mp_h_acc_fruitbowl_02]',
+    ],
+	
+	['tomato'] = {
+        [1] = 'ex_mp_h_acc_fruitbowl_02',
+    ],
+	
+	['coconut'] = {
+        [1] = 'ex_mp_h_acc_fruitbowl_01',
+    ],
+	
+	['cherry'] = {
+        [1] = 'ex_mp_h_acc_fruitbowl_02',
+    ],
+	
+	['blueberry'] = {
+        [1] = 'ex_mp_h_acc_fruitbowl_01',
+    ],
+	
+	['strawberry'] = {
+        [1] = 'ex_mp_h_acc_fruitbowl_02',
+    ],
+	
+	['apple'] = {
+        [1] = 'apa_mp_h_acc_fruitbowl_01',
+    ],
+	
+	['orange'] = {
+        [1] = 'ex_mp_h_acc_fruitbowl_02',
+    ],
+--crops
+
+	['lettuce'] = {
+        [1] = 'prop_veg_crop_03_cab',
+    ],
+	
+	['maize'] = {
+        [1] = 'v_res_fa_tincorn',
+    ],
+	
+	['barley'] = {
+        [1] = 'prop_veg_crop_05',
+    ],
+--Jim-Recycle Items
+	
+	['kq_outfitbag'] = {
+        [1] = 'prop_nigel_bag_pickup',
+    ],
+	
+	['can'] = {
+        [1] = 'v_res_tt_can03',
+    ],
+	
+	['recyclablematerial'] = {
+        [1] = 'prop_woodpile_03a',
+    ],
+--jim-mining stuff
+
+	['can'] = {
+        [1] = 'v_res_tt_cancrsh02',
+    ],
+	
+	['bottle'] = {
+        [1] = 'watercooler_bottle001',
+    ],
+	
+	['pan'] = {
+        [1] = 'prop_copper_pan',
+    ],
+	
+	['drillbit'] = {
+        [1] = 'gr_prop_gr_part_drill_01a',
+    ],
+	
+	['miningdrill'] = {
+        [1] = 'hei_prop_heist_drill',
+    ],
+	
+	['pickaxe'] = {
+        [1] = 'prop_tool_pickaxe',
+    ],
+	
+	['silveringot'] = {
+        [1] = 'ex_office_swag_silver',
+    ],
+	
+	['goldingot'] = {
+        [1] = 'hei_prop_heist_gold_bar',
+    ],
+	
+	['silverearring'] = {
+        [1] = 'p_tmom_earrings_s',
+    ],
+	
+	['silverchain'] = {
+        [1] = 'p_stretch_necklace_s',
+    ],
+	
+	['goldchain'] = {
+        [1] = 'p_ortega_necklace_s',
+    ],
+--Repair Parts
+
+	['newoil'] = {
+        [1] = 'prop_oilcan_01a',
+    ],
+	
+	['carbattery'] = {
+        [1] = 'prop_car_battery_01',
+    ],
+	
+	['sparetire'] = {
+        [1] = 'prop_rub_wheel_01',
+    ],
+--Cosmetics 
+
+	['underglow_controller'] = {
+        [1] = 'v_res_tre_remote',
+    ],
+	
+	['customplate'] = {
+        [1] = 'p_num_plate_03',
+    ],
+	
+	['hood'] = {
+        [1] = 'imp_prop_impexp_bonnet_02a',
+    ],
+	
+	['spoiler'] = {
+        [1] = 'imp_prop_impexp_spoiler_03a',
+    ],
+	
+	['bumper'] = {
+        [1] = 'imp_prop_impexp_front_bumper_02a',
+    ],
+	
+	['exhaust'] = {
+        [1] = 'imp_prop_impexp_exhaust_02',
+    ],
+	
+	['seat'] = {
+        [1] = 'prop_ejector_seat_01',
+    ],
+	
+	['rims'] = {
+        [1] = 'prop_wheel_rim_03',
+    ],
+	
+	['paintcan'] = {
+        [1] = 'ng_proc_paintcan01a',
+    ],
+	
+	['tires'] = {
+        [1] = 'prop_rub_wheel_01',
+    ],
+--Vehicle Extra Damage Items
+
+	['cables1'] = {
+        [1] = 'prop_cablespool_06',
+    ],
+	
+	['cables2'] = {
+        [1] = 'prop_cablespool_03',
+    ],
+	
+	['cables3'] = {
+        [1] = 'prop_tool_cable02',
+    ],
+	
+	['fueltank1'] = {
+        [1] = 'prop_byard_gastank01',
+    ],
+	
+	['cylind2'] = {
+        [1] = 'prop_gascyl_02a',
+    ],
+	
+	['cylind3'] = {
+        [1] = 'prop_gascyl_01a',
+    ],
+-- Other Tools
+
+	['casinochips'] = {
+        [1] = 'ng_proc_concchips01',
+    ],
+	
+	['moneybag'] = {
+        [1] = 'prop_big_bag_01',
+    ],
+	
+	['parachute'] = {
+        [1] = 'p_parachute_s_shop',
+    ],
+	
+	['lighter'] = {
+        [1] = 'ex_prop_exec_lighter_01',
+    ],
+	
+	['markedbills'] = {
+        [1] = 'bkr_prop_cutter_moneystack_01a',
+    ],
+	
+	['labkey'] = {
+        [1] = 'bkr_prop_jailer_keys_01a',
+    ],
+	
+	['printerdocument'] = {
+        [1] = 'ng_proc_paper_news_rag',
+    ],
+	
+	['newscam'] = {
+        [1] = 'prop_pap_camera_01',
+    ],
+	
+	['newsmic'] = {
+        [1] = 'p_ing_microphonel_01',
+    ],
+--sea tools 
+
+	['dendrogyra_coral'] = {
+        [1] = 'prop_coral_pillar_01',
+    ],
+	
+	['antipatharia_coral'] = {
+        [1] = 'prop_coral_kelp_04',
+    ],
+	
+	['diving_gear'] = {
+        [1] = 'p_s_scuba_tank_s',
+    ],
+	
+	['diving_fill'] = {
+        [1] = 'p_michael_scuba_tank_s',
+    ],
+--Firework Tools
+
+	['firework1'] = {
+        [1] = 'ind_prop_firework_03',
+    ],
+	
+	['firework2'] = {
+        [1] = 'w_lr_firework_rocket',
+    ],
+	
+	['firework3'] = {
+        [1] = 'ind_prop_firework_01',
+    ],
+	
+	['firework4'] = {
+        [1] = 'ind_prop_firework_02',
+    ],
 }
 
 -- https://docs.fivem.net/docs/game-references/controls/
 -- Use the input index for the "input" value
 Config.keybinds = {
     openMenu = {
-        enabled = true,
+        enabled = false,
         key = 'F4',
     },
     pickup = {
