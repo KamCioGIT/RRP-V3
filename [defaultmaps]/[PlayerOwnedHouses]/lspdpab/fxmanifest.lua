811b56c8a2271444e369612089201e30955acdad
Skutela32 <kwebalkop@gmail.com>
2024-02-15 14:32:08 +0000

    Houses + Construction Replace

---
+++ [defaultmaps]/[PlayerOwnedHouses]/lspdpab/fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+this_is_a_map 'yes'
+file "sp_manifest.ymt"
+file "audioconfig/game.dat151.rel"
+data_file "SCENARIO_POINTS_OVERRIDE_PSO_FILE" "sp_manifest.ymt"
+
+author'tidemo'
