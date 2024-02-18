2ad05a0263301c014a50837e09e90bdf768e2078
Skutela32 - Laptop <96396382+Skutela32@users.noreply.github.com>
2024-02-18 22:53:54 +0000

    Update engineswappoints.lua

--- [standalone]/[TI]/ti_mechEngineer/data/engineswappoints.lua
+++ [standalone]/[TI]/ti_mechEngineer/data/engineswappoints.lua
@@ -8,5 +8,9 @@ engineSwapPoints = {
     point1 = {
         position = vector3(-582.95, -917.53, 23.89),
         heading = 181.0,
+    },
+	point2 = {
+        position = vector3(561.73, -200.09, 50.68),
+        heading = 181.0,
     }
 } 
