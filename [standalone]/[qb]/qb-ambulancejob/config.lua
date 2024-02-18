c11e3b1964d914413cda8717b19f98018c24340a
Skutela32 - Laptop <96396382+Skutela32@users.noreply.github.com>
2024-02-18 03:58:05 +0000

    Update config.lua

--- [standalone]/[qb]/qb-ambulancejob/config.lua
+++ [standalone]/[qb]/qb-ambulancejob/config.lua
@@ -115,6 +115,10 @@ Config.Locations = {                -- Edit the various interaction points for p
 Config.AuthorizedVehicles = { -- Grade is key, don't add same vehicle in multiple grades. Higher rank can see lower
     [0] = {
         ['ambulance'] = 'Ambulance'
+    },
+	[1] = {
+        ['530ambo'] = '223M',
+        ['volvoxc90ambo'] = 'XC90',
     }
 }
 
