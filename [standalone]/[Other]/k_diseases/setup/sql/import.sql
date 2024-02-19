3cdc4f031dc8e1b4e5a1638da4effed2dbe64059
Skutela32 - Laptop <96396382+Skutela32@users.noreply.github.com>
2024-02-19 01:41:50 +0000

    removed laggy/buggy scripts

--- [standalone]/[Other]/k_diseases/setup/sql/import.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE `player_sickness` (
-	`citizenid` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
-	`sickness` TEXT NOT NULL COLLATE 'utf8_general_ci',
-	PRIMARY KEY (`citizenid`) USING BTREE
-);
