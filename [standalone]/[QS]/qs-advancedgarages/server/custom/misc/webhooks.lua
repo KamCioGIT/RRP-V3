a5e4a49cba4d405333799d6fa868bb1e6dbef007
Skutela32 - Laptop <96396382+Skutela32@users.noreply.github.com>
2024-02-22 23:05:08 +0000

    Garage Swap

---
+++ [standalone]/[QS]/qs-advancedgarages/server/custom/misc/webhooks.lua
@@ -0,0 +1,15 @@
+--[[
+    Welcome webhooks setup!
+    Here you will have the link to configure the admin webhooks,
+    you can modify them from server/custom/misc/SetInventoryData.lua.
+]]
+
+Webhooks = Webhooks or {}
+
+Webhooks = {
+    ['take_vehicle'] = 'https://discord.com/api/webhooks/1197804984125890600/KUEdmg-hBz81_vZNEtFHfVk6OL0EO23IcTMzwolnLvQu_8Os56a88-vk4xs-IGVMTvgK',
+    ['save_vehicle'] = 'https://discord.com/api/webhooks/1197804984125890600/KUEdmg-hBz81_vZNEtFHfVk6OL0EO23IcTMzwolnLvQu_8Os56a88-vk4xs-IGVMTvgK',
+    ['recovery_vehicle'] = 'https://discord.com/api/webhooks/1197804984125890600/KUEdmg-hBz81_vZNEtFHfVk6OL0EO23IcTMzwolnLvQu_8Os56a88-vk4xs-IGVMTvgK',
+    ['impound_vehicle'] = 'https://discord.com/api/webhooks/1197804984125890600/KUEdmg-hBz81_vZNEtFHfVk6OL0EO23IcTMzwolnLvQu_8Os56a88-vk4xs-IGVMTvgK',
+    ['give_vehicle'] = 'https://discord.com/api/webhooks/1197804984125890600/KUEdmg-hBz81_vZNEtFHfVk6OL0EO23IcTMzwolnLvQu_8Os56a88-vk4xs-IGVMTvgK',
+}
